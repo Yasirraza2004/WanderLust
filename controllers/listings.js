00188913b9b7b62a09f6598654136d647418e2d2
Yasirraza2004 <yashirraza2004@gmail.com>
2026-08-06 01:37:01 +0530

    Review delete button updated

--- controllers/listings.js
+++ controllers/listings.js
@@ -36,7 +36,7 @@ module.exports.showListing = async (req, res) => {
       req.flash("error", "Listing you requested for does not exist!");
       return res.redirect("/listings");
     }
-    console.log(listing);
+    // console.log(listing);
     res.render("listings/show.ejs", { listing });
   };
 
@@ -57,7 +57,7 @@ module.exports.showListing = async (req, res) => {
     newListing.image = {url, filename};
     newListing.geometry = response.body.features[0].geometry;
     let savedListing = await newListing.save();
-    console.log(savedListing);
+    // console.log(savedListing);
 
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
--- controllers/reviews.js
+++ controllers/reviews.js
@@ -6,6 +6,7 @@ module.exports.createReview = async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
     newReview.author = req.user._id;
+    // console.log(newReview.author._id);
     listing.reviews.push(newReview);
 
     await newReview.save();
